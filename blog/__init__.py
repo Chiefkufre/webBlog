bb5a78fe3ba6c40297421553c2d75ed5034f23f1
chiefkufre1 <kufhub@gmail.com>
2022-05-04 08:14:04 +0100

    chore: add flask main start component

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,1 +1,11 @@
-ECHO is on.
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = 'hdjds njdj'
+    app.config['SESSION_TYPE'] = 'file system'
+    
+    
+    
+    return app
