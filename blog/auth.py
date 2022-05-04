7ead9b49bbd3b1a2c6260ce863e79bfec04166b4
chiefkufre1 <kufhub@gmail.com>
2022-05-04 08:42:48 +0100

    chore: add blueprint and views for the authentication pages

--- blog/auth.py
+++ blog/auth.py
@@ -1,1 +1,40 @@
-ECHO is on.
+from flask import Blueprint, redirect, render_template, flash, url_for
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    
+    render_template('login.html')
+    
+
+
+@auth.route('/logout')
+def logout():
+    
+    render_template(url_for('login.html'))
+    
+
+@auth.route('/sign-up')
+def sign_up():
+    
+    render_template('sign_up.html')
+    
+
+@auth.route('/write')
+def post():
+    
+    render_template('posts.html')
+    
+    
+
+@auth.route('write/edit/<int:id>')
+def edit(id):
+    
+    render_template('edit.html')
+    
+
+@auth.route('/write/delete/<int:id>')
+def delete(id):
+    
+    redirect(url_for('auth.post'))
