afee42d765a82f519546f58b7b6d806ea61cd89f
chiefkufre1 <kufhub@gmail.com>
2022-05-04 16:29:22 +0100

    chore: create database model

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,13 @@
-ECHO is on.
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+from datetime import datetime
+
+
+class BlogContent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    author = db.Column(db.String(20), nullable=False, default='N/A')
+    content = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    
