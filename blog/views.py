c66b8835a3954f8740f5ac80aaff641f506d4189
chiefkufre1 <kufhub@gmail.com>
2022-05-04 08:29:30 +0100

    chore: create blueprint and route form for home, contact and about pages

--- blog/views.py
+++ blog/views.py
@@ -1,1 +1,22 @@
-ECHO is on.
+from flask import Blueprint, redirect, render_template, flash, url_for
+
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/')
+def home():
+    
+    render_template('home.html')
+    
+
+@views.route('/contact')
+def contact():
+    
+    render_template('contact.html')
+    
+
+@views.route('/about')
+def about():
+    
+    render_template('about.html')
