53a8d6459bc6c86e64658c7974e3a316fef2b32c
chiefkufre1 <kufhub@gmail.com>
2022-05-04 08:52:51 +0100

    chore: registerd views and blueprint in the flask factory

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, Blueprint
 
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = 'hdjds njdj'
     app.config['SESSION_TYPE'] = 'file system'
+    
+    from .views import views
+    from .auth import auth
+    
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    
     
     
     
