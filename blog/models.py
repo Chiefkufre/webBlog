c201c6f3f8c97632b47b549db66bd78c6d53d20b
chiefkufre1 <kufhub@gmail.com>
2022-05-05 22:42:20 +0100

    fix: database model syntax error

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,5 @@
 from . import db
 from flask_login import UserMixin
-from sqlalchemy.sql import func
 from datetime import datetime
 
 
@@ -10,4 +9,24 @@ class BlogContent(db.Model):
     author = db.Column(db.String(20), nullable=False, default='N/A')
     content = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False)
+    full_name = db.Column(db.String(200), nullable=False, default='N/A')
+    password = db.Column(db.String(50), nullable=False)
+    content = db.relationship('BlogContent')
+    
+    
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False)
+    full_name = db.Column(db.String(200), nullable=False, default='N/A')
+    subject = db.Column(db.String(200), nullable=False)
+    message = db.Column(db.Text,nullable=False )
+   
+
     
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, redirect, render_template, flash, url_for
+from flask import Blueprint, redirect, render_template, flash, url_for, request
+from . import db
+from .models import BlogContent, User, Contact
 
 
 views = Blueprint('views', __name__)
@@ -10,8 +12,20 @@ def home():
     return render_template("home.html")
     
 
-@views.route('/contact')
+@views.route('/contact', methods=['GET','POST'])
 def contact():
+    if request.method == 'POST':
+        full_name = request.form.get('name')
+        email = request.form.get('email')
+        subject = request.form.get('subject')
+        message = request.form.get('message')
+        
+        new_message = Contact(full_name=full_name, email=email,subject=subject, message=message)
+        db.session.add(new_message)
+        db.session.commit()
+        flash('message sent', category = 'success')
+        redirect(url_for('views.contact'))
+        
     
     return render_template('contact.html')
     
